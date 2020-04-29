d901dc89eace96be95da159dd290619534a2ed8e
Codders <Codders@192.168.0.101>
2020-04-29 14:12:11 +0300

    COMPLETE

---
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/MyUserDetailsService.java
@@ -0,0 +1,30 @@
+package io.javabrains.springsecurityjwt;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import io.javabrains.springsecurityjwt.filters.MyUserDetails;
+import io.javabrains.springsecurityjwt.filters.User;
+import io.javabrains.springsecurityjwt.filters.UserRepository;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	 @Autowired
+	 UserRepository userRepository;
+	 
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+//        return new User("foo", "foo", 
+//                new ArrayList<>());
+    	Optional<User> user = userRepository.findByUserName(userName);
+    	user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+    	return user.map(MyUserDetails::new).get();
+    
+    }
+}
---
+++ spring-security-jwt/src/main/java/io/javabrains/springsecurityjwt/filters/UserRepository.java
@@ -0,0 +1,9 @@
+package io.javabrains.springsecurityjwt.filters;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}
